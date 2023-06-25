279834470409736ca7cbd49832ac0e64cc84d4e4
yingxiongyanjin <2575890956@qq.com>
2023-06-25 15:15:11 +0800

    cu program and py program and update readme

--- Julia_Origin_Analysis.cu
+++ Julia_Origin_Analysis.cu
@@ -10,30 +10,33 @@ struct cuComplex
     float i;
 
     // 初始化方法
-    cuComplex(float a, float b):r(a),i(b) {}
+    __device__ cuComplex(float a, float b):r(a),i(b) {}
 
     // 计算复数的模
-    float magnitude2(void) {return r*r + i*i;}
+    __device__ float magnitude2(void) {return r*r + i*i;}
 
     // 规定复数的乘法和加法
-    cuComplex operator*(const cuComplex &a){
+    __device__ cuComplex operator*(const cuComplex &a){
         return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
     }
-    cuComplex operator+(const cuComplex &a){
+    __device__ cuComplex operator+(const cuComplex &a){
         return cuComplex(r+a.r, i+a.i);
     }
 };
 
+__device__
 int julia
 (
     int x, int y,
     int DIM,
-    float scale, cuComplex c, int step, float inf
+    float scale, int step, float inf
 ){
     // 首先通过缩放、平移将x和y的值控制在[-scale,scale]之间
     // 生成复数平面上的一个点
     float jx = scale * (float)(DIM/2 - x)/(DIM/2);
     float jy = scale * (float)(DIM/2 - y)/(DIM/2);
+
+    cuComplex c(-0.8,0.156);               // 迭代式中的常量，通过改变它可以获得不同的Julia集图案
     cuComplex a(jx, jy);
 
     // 进行迭代，判断点集是否发散
@@ -46,22 +49,38 @@ int julia
     return 1;
 }
 
+__global__
 void kernel( 
     int *bitmap,
     int DIM,
-    float scale, cuComplex c, int step, float inf
+    float scale, int step, float inf
 )
 {
     for(int y = 0; y < DIM; y++){
         for(int x = 0; x < DIM; x++){
 
             // 判断是否为Julia集中的点
-            int juliaValue = julia(x, y, DIM, scale,c,step,inf);
+            int juliaValue = julia(x, y, DIM, scale,step,inf);
 
             // 如果是Julia集中的点，将其标记为白色
             // 如果不是Julia集中的点，则将其标记为红色
             int offset = x + y * DIM;
             bitmap[offset] = 255 * juliaValue;
+        }
+    }
+}
+
+void checkOut(
+    int &sum,
+    int *bitmap,int DIM
+)
+{
+    sum = 0;
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
+            if(bitmap[i*DIM+j] != 0 ){
+                sum++;
+            }
         }
     }
 }
@@ -73,35 +92,40 @@ int main( void ){
     start = clock(); // 记录开始时间
 
     // 本程序的控制参数列表
-    const int DIM = 6000;                 // 二维数组的大小
+    const int DIM = 10000;                 // 二维数组的大小
     int *bitmap;                           // 存储位图数据,采用灰度图存储结果
 
     const float scale = 1.5;               // 控制x,y的值域，具体的说先通过 f(x) = 1 - 2x/DIM 将值映射到[-1,1],再通过该因子进行放缩
-    cuComplex c(-0.8, 0.156);              // 迭代式中的常量，通过改变它可以获得不同的Julia集图案
     const int step = 200;                  // 控制迭代次数
     const float inf = 1000;                // 判断点是否发散的设置的无穷大值
     
     // 动态分配空间
     int n = DIM * DIM;
     const int size = n*sizeof(int);
-    bitmap = (int*)malloc(size);
+
+    // CUDA内存管理办法
+    cudaMallocManaged(&bitmap, size);
 
     // 核心代码
-    kernel(bitmap,DIM,scale,c,step,inf);
+    kernel<<<1,1>>>(bitmap,DIM,scale,step,inf);
 
-    // 为了在Nvidia提供的云平台上输出结果，这里直接将bitmap打印出来
-    // 再讲其保存到result.txt文件中
-    // 另外提供了python代码将结果可视化
-    // 当DIM大于10000时，不建议使用该方式进行输出
-    for (int i = 0; i < DIM; i++) {
-        for (int j = 0; j < DIM; j++) {
-            printf("%d ", bitmap[i*DIM+j]);
-        }
-        printf("\n");
-    }
+    cudaError_t Err;
+    cudaError_t asyncErr;
+
+    Err = cudaGetLastError();
+    if(Err != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(Err));
+
+    // 阻断，并进行同步
+    asyncErr = cudaDeviceSynchronize();
+    if(asyncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(asyncErr));
+
+    int sum = 0;
+    checkOut(sum,bitmap,DIM);
 
     // 释放内存
-    free(bitmap);
+    cudaFree(bitmap);
+
+    printf("done! %d\n",sum);
 
     end = clock(); // 记录结束时间
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # JuliaGenerateProgram
-用于生成Julia集的简单项目，但是最终的结果是很美，很令人兴奋的。  
+用于生成Julia集的简单项目，但是最终的结果是很美、很令人兴奋的。  
 ## 简单
 1. **原理容易实现**   
 通过下式反复迭代判断该复平面点是否发散即可生成Julia集：
@@ -9,7 +9,7 @@ $$
 2. **程序结构清晰**  
 通过调整以下参数就可以完全控制程序的走向：
 ```C
-const int DIM = 6000;                 // 二维数组的大小
+const int DIM = 3000;                 // 二维数组的大小
 int *bitmap;                           // 存储位图数据,采用灰度图存储结果
 
 const float scale = 1.5;               // 控制x,y的值域，
@@ -20,7 +20,7 @@ const int step = 200;                  // 控制迭代次数
 const float inf = 1000;                // 判断点是否发散的设置的无穷大值
 ```
 ## 美与令人兴奋的结果
-本文只提供了python的可视化代码
+本文只提供了python的可视化代码,也可以查看直接运行visual.ipynb文件。
 ```python
 import numpy as np
 import matplotlib.pyplot as plt
